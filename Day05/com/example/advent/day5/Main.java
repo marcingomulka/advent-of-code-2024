82b28f09a7faa3110c0b4dfae70b52efd2c23373
marcin-gomulka <magomulka@gmail.com>
2024-12-05 06:55:28 +0100

    Solution: Day5

--- Day05/com/example/advent/day5/Main.java
+++ Day05/com/example/advent/day5/Main.java
@@ -1,14 +1,14 @@
 package com.example.advent.day5;
 
-import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.HashMultimap;
-import io.vavr.collection.Multimap;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 
 
