c34b86a532f9edbb33c4ece1cf0f309dcda21d79
marcin-gomulka <magomulka@gmail.com>
2024-12-10 14:17:28 +0100

    Ugly solution to Day9

--- Day09/com/example/advent/day9/Main.java
+++ Day09/com/example/advent/day9/Main.java
@@ -1,21 +1,18 @@
 package com.example.advent.day9;
 
-import org.checkerframework.checker.units.qual.A;
-import org.xml.sax.helpers.AttributeListImpl;
-
+import javax.swing.text.html.Option;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 
 public class Main {
 
     static class File {
-
         int id;
         int len;
         Optional<File> prev = Optional.empty();
@@ -73,8 +70,7 @@ public class Main {
             pos++;
         }
         diskPart2Last = curr;
-        //System.out.println(disk.size());
-        //System.out.println(disk.subList(94776, 94796));
+
         int index = disk.size() -1;
         int start = 0;
         while (index >= 0 && index > start) {
@@ -98,46 +94,92 @@ public class Main {
             }
         }
         System.out.println("Part1: " + resultPart1);
-        System.out.println(disk.contains(-1));
-        System.out.println(disk.size());
 
-        //System.out.println(diskPart2);
-        File files = diskPart2;
-        File empties = diskPart2Last;
-        while (files.prev.isPresent() && files.prev.get() != empties.next.orElse(new File(-2,-2))) {
-            int prevIndex = files.previousIndex();
-            File file = files.previous();
+        Optional<File> files = Optional.of(diskPart2Last);
+        while (files.isPresent()) {
+            File file = files.get();
+            boolean moved = false;
             if (file.id >= 0) {
-                while (empties.hasNext() && diskPart2.get(empties.nextIndex()).id >= 0) {
-                    empties.next();
-                }
-                if (empties.hasNext() && diskPart2.get(empties.nextIndex()).id < 0 && prevIndex > empties.nextIndex()) {
-                    File empty = empties.next();
-                    if (empty.len >= file.len) {
-                        empty.len -= file.len;
-                        empties.remove();
-                        empties.add(file);
-                        if (empty.len > 0) {
-                            empties.add(empty);
-                        }
+                Optional<File> empties = Optional.of(diskPart2);
+                while (empties.isPresent() && !emptyFits(empties.get(), file) && empties.get().next.isPresent()) {
+                    if (empties.get() == file) {
+                        break;
                     }
+                    empties = empties.get().next;
+                }
+                if (empties.isPresent() && emptyFits(empties.get(), file) && empties.get() != file) {
+                    File oldFile = new File(-1, file.len);
+                    oldFile.next = file.next;
+                    oldFile.prev = file.prev;
+                    file.prev.ifPresent(p -> p.next = Optional.of(oldFile));
+                    file.next.ifPresent(p -> p.prev = Optional.of(oldFile));
+
+                    File empty = empties.get();
+
+                    empty.len -= file.len;
+
+                    file.prev = empty.prev;
+                    empty.prev.ifPresent(p -> p.next = Optional.of(file));
+                    if (empty.len > 0) {
+                        file.next = Optional.of(empty);
+                        empty.prev = Optional.of(file);
+                    } else {
+                        file.next = empty.next;
+                        empty.next.ifPresent(p -> p.prev = Optional.of(file));
+                    }
+                    files = oldFile.prev;
+                    moved = true;
                 }
             }
+            if (!moved) {
+                files = files.get().prev;
+            }
+            compactEmpties(diskPart2);
         }
-        System.out.println(diskPart2);
+        System.out.println();
         long sumPart2 = 0L;
         long offset = 0L;
-        for (File file : diskPart2) {
-            for (int i = 0; i < file.len; i++) {
-                sumPart2 += (offset+i) * file.id;
+        Optional<File> file = Optional.of(diskPart2);
+        while (file.isPresent()) {
+            File current = file.get();
+            for (int i = 0; i < current.len; i++) {
+                if (current.id >= 0) {
+                    sumPart2 += (offset + i) * current.id;
+                }
             }
-            offset += file.len;
+            offset += current.len;
+            file = current.next;
         }
         System.out.println("Part2: " + sumPart2);
-        //System.out.println(disk.subList(disk.size()-20, disk.size()));
-        //System.out.println(disk.subList(0, 20));
-
     }
+
+    private static void compactEmpties(File diskPart2) {
+        Optional<File> curr = Optional.of(diskPart2);
+        while (curr.isPresent()) {
+            File file = curr.get();
+            if (file.id == -1 && file.next.isPresent() && file.next.get().id == -1) {
+                file.len += file.next.get().len;
+                file.next.get().next.ifPresent(p -> p.prev = Optional.of(file));
+                file.next = file.next.get().next;
+
+            }
+            curr = file.next;
+        }
+    }
+
+    private static boolean emptyFits(File empty, File file) {
+        return empty.id == -1 && empty.len >= file.len;
+    }
+
+    private static void printList(File diskPart2) {
+        System.out.print(diskPart2 + ", ");
+        while (diskPart2.next.isPresent()) {
+            diskPart2 = diskPart2.next.get();
+            System.out.print(diskPart2 + ", ");
+        }
+        System.out.println();
+    }
+
     private static List<String> readInput() throws IOException {
         List<String> lines;
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
